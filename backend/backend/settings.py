6f26e4661785347dcb7a9e8cdf002f5616eacc59
Nguyen Le Bao Duy <duy2151220289@gmail.com>
2026-05-29 08:21:51 +0700

    feat: configure dynamic PostgreSQL database support via dj_database_url

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,5 +1,6 @@
 import os
 import mimetypes
+import dj_database_url
 from pathlib import Path
 
 mimetypes.add_type("application/javascript", ".js", True)
@@ -56,10 +57,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
